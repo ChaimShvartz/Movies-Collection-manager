36d155fe15806bdc1b135ebcce9053292ad3dae0
Chaim <c0507634153@gmail.com>
2026-07-03 15:13:23 +0300

    Fix: add handling when data dosen't exists

--- services/file-service.js
+++ services/file-service.js
@@ -3,17 +3,18 @@ import fs from "fs/promises";
 const path = "data/movies.json";
 
 export async function loadMoviesAsync() {
-    
     try {
-        return await fs.readFile(path, "utf8").then(JSON.parse);
+        const data = await fs.readFile(path, "utf8");
+        return JSON.parse(data);
     } catch (err) {
         console.error(err.message);
+        return [];
     }
 }
 
-export async function dumpMoviesAsync(data) {
+export async function dumpMoviesAsync(movies) {
     try {
-        return await fs.writeFile(path, JSON.stringify(data, null, 2));
+        await fs.writeFile(path, JSON.stringify(movies, null, 2), "utf8");
     } catch (err) {
         console.error(err.message);
     }
